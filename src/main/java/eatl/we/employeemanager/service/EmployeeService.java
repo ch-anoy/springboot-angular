f412f11a4ef17db9504824bfd2e87a4f7c5bfe32
anoy chowdhury <anoy@eatlbd.com>
2021-02-25 17:26:24 +0600

    created model,database connection,created services

---
+++ src/main/java/eatl/we/employeemanager/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package eatl.we.employeemanager.service;
+
+import eatl.we.employeemanager.model.Employee;
+import eatl.we.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.List;
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo ;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo=employeeRepo;
+    }
+    public Employee addEmployee(Employee employee)
+    {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+    public List<Employee>findAllEmployees()
+    {
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee)
+    {
+        return employeeRepo.save(employee);
+    }
+    public Employee findEmployeeById(Long id)
+    {
+        return employeeRepo.findEmployeeById(id);
+    }
+    public void deleteEmployee(Long id)
+    {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
